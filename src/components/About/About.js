79a32ee2b934a7db9d0625b7dea9d5ddd3aebb7e
arhee <arheeee@gmail.com>
2018-06-15 17:24:18 -0700

    changed bio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -28,10 +28,13 @@ class About extends React.Component {
                                         I worked in finance for over 4 years before deciding to change career paths to my true passion: Software Development. 
                                         <br/>
                                         <br/>
-                                        My work experience spans from large corporations such as NBC Universal to a mid size insurance carrier, ICW Group, and even a smaller size boutique mortgage lender, Global Equity Finance. Working at these various companies has given me exposure to many different work environments, corporate cultures, and work systems. One of the most important things I learned from my four years in finance was clear and consistent communication, whether it be with clients, co-workers, superiors, etc., which is something that sets me apart from many developers.
+                                        One of the most important things I learned from my four years in finance was clear and consistent communication, whether it be with clients, co-workers, etc., which has become a strength of mine even in development.
                                         <br />
                                         <br/>
                                         My reason for switching careers was simply due to my unbridled passion for software development, which has been validated by my ability to quickly learn new languages and seamlessly understand programming fundamentals.
+                                        <br/>
+                                        <br/>
+                                        Creating and designing websites and applications is something I truly love to do, so let's work together and make something great!
                                 </div>
                                 </Col>
                             </div>
